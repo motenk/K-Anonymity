fd627ae7069826566fe75f7f1767b2245da3c726
Moten <motenkingsmill@hotmail.com>
2017-10-09 12:30:59 +1100

    Update

--- src/kanonymity/KAnon.java
+++ src/kanonymity/KAnon.java
@@ -1,36 +1,25 @@
-package kanonymity;
-
 import java.util.*;
 import java.io.*; 
 
 public class KAnon
 {
-	/*
-		Method: main
-		Preconditions: Existing CSV file
-		Postconditions: K-Anonymous table generated
-	*/
+	//Preconditon: 	Valid CSV file
+	//Postcondtion:	K-Anonymous table generated and output
+	//Status:		Coded and efficient
+	//Written by:	Moten
 	public static void main(String[] args) 
 	{
 		Scanner scanner = new Scanner(System.in); 
 		boolean flag = false;
 		int input = 0;
 
-
-		System.out.println("Processing File... Please wait.");
-		KAnonMethods table = new KAnonMethods(importFile(new File(args[0])));
-		if (table != null)
-			System.out.println("File successfully imported! Current K-Anonymous Value: "+table.evaluateKAnon());
-		else
-			return;
-
-		while (!flag)
+		while (!flag) //Get K-Value
 		{
 			try
 			{
 				System.out.println("Please enter a value for K:");
 				input = scanner.nextInt();
-				if (input < 20 && input > 0)
+				if (input < 50 && input > 0)
 					break;
 			}
 			catch (InputMismatchException e)
@@ -42,11 +31,21 @@ public class KAnon
 
 		}
 
-		ArrayList<Tuple> output = table.makeKAnon(input);
+		System.out.println("Processing File... Please wait.");
+		long millis = System.currentTimeMillis(); // Start run timer
+		KAnonMethods table = new KAnonMethods(importFile(new File(args[0])), input); //De
+
+		table.makeKAnon();
+		ArrayList<Tuple> output = table.getOutput();
+
+		if (table != null)
+			System.out.println("File successfully imported! Current K-Anonymous Value: "+table.getCurrentK());
+		else
+			return;
 
 		if (output != null) 
 		{
-			System.out.println("Table Processed! Data Loss Value: "+table.getDataLoss());
+			System.out.println("Table Processed! Data Loss Value: "+table.getDataLoss()+"%");
 			String[] parts = args[0].split("\\.");
 			if(outputFile(output, (parts[0]+"_output.csv")))
 				System.out.println("File successfully ouput!");
@@ -55,9 +54,16 @@ public class KAnon
 		}
 		else
 			System.out.println("Error converting file.");
+
+		double runtime = (System.currentTimeMillis()-millis)/1000.0;
+		System.out.println("Total run time: "+runtime+" seconds");
 		System.out.println("Program Complete. Exiting.");
 	}
 
+	//Preconditon: 	Valid CSV file
+	//Postcondtion:	Array List of tuples generated
+	//Status:		Coded and efficient
+	//Written by:	Moten
 	private static ArrayList<Tuple> importFile(File file)
 	{
 		Scanner console;
@@ -73,7 +79,6 @@ public class KAnon
 			System.out.println("Error importing file. Please try again.");
 			return null;
 		}
-		//Process column names here
 		while(console.hasNextLine()) //Import file into data structure
 		{
 			ArrayList<String> tupleInput = new ArrayList<String>();
@@ -88,6 +93,10 @@ public class KAnon
 		return output;
 	}
 
+	//Preconditon: 	Valid array list of tuples and title
+	//Postcondtion:	K-Anonymous saved to file
+	//Status:		Coded and efficient
+	//Written by:	Moten
 	private static boolean outputFile(ArrayList<Tuple> input, String title)
 	{
 		try
@@ -95,7 +104,7 @@ public class KAnon
 		    PrintWriter writer = new PrintWriter(title, "UTF-8");
 		    for (int i = 0; i < input.size(); i++)
 		    {
-		    	writer.println(input.get(i).printTuple());
+		    	writer.println(input.get(i).toString());
 		    }
 		    writer.close();
 		} 
--- src/kanonymity/Tuple.java
+++ src/kanonymity/Tuple.java
@@ -1,12 +1,15 @@
-package kanonymity;
-
 import java.util.*;
 
 public class Tuple
 {
 	private String[] values;
 	private int size;
+	private long hash;
 
+	//Preconditon: 	Valid array list of string input
+	//Postcondtion:	Tuple initialised
+	//Status:		Coded and efficient
+	//Written by:	Moten
 	public Tuple(ArrayList<String> input)
 	{
 		size = input.size();
@@ -15,18 +18,40 @@ public class Tuple
 			values[i] = input.get(i);
 	}
 
+	//Preconditon: 	Tuple initialised
+	//Postcondtion:	Value of field i returned
+	//Status:		Coded and efficient
+	//Written by:	Moten
 	public String get(int i)
 	{
 		return values[i];
 	}
 
-	public String printTuple()
+	//Preconditon: 	Tuple initialised
+	//Postcondtion:	Printed tuple returned
+	//Status:		Coded and efficient
+	//Written by:	Moten
+	public String toString()
 	{
-		String output = "";
-		for (int i = 0; i < size; i++)
-		{
-			output += values[i]+", ";
-		}
+		String output = values[0];
+		for (int i = 1; i < size; i++)
+			output += ", "+values[i];
+		return output;
+	}
+
+	//Preconditon: 	Tuple initialised
+	//Postcondtion:	Hash value for tuple returned
+	//Status:		Coded 
+	//Written by:	Moten
+	public int getHash() //Gives inaccurate hashes
+	{
+		String total = "";
+		int output = 0;
+		for (int i = 0; i < values.length; i++)
+			total += "$"+values[i]; 
+		for (int i = 0; i < total.length(); i++)
+			output = output + (int)total.charAt(i);
+
 		return output;
 	}
 }
